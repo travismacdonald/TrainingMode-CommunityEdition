b11394c5b82f619521f1d12c138ef5d94870b7fe
Alex Harrison <alexander.harrison.r@gmail.com>
2024-07-07 11:30:09 -0600

    cpu neutral jump by default

--- patch/events/lab/source/lab.c
+++ patch/events/lab/source/lab.c
@@ -1431,7 +1431,7 @@ static EventOption LabOptions_CPU[] = {
     {
         .option_kind = OPTKIND_STRING,                                                      // the type of option this is; menu, string list, integer list, etc
         .value_num = sizeof(LabValues_CounterAir) / 4,                                      // number of values for this option
-        .option_val = 1,                                                                    // value of this option
+        .option_val = 4,                                                                    // value of this option
         .menu = 0,                                                                          // pointer to the menu that pressing A opens
         .option_name = "Counter Action (Air)",                                              // pointer to a string
         .desc = "Select the action to be performed after an\nairborne CPU's hitstun ends.", // string describing what this option does
