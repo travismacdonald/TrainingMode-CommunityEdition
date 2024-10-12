9a23e22abc5a998cc4b91bcf5eade1d837b0cfcd
Alex Harrison <AlexanderHarrisonR@gmail.com>
2024-10-12 02:37:24 -0600

    /s /e /y

--- build_windows.bat
+++ build_windows.bat
@@ -28,46 +28,43 @@ if not exist "C:/devkitPro/devkitPPC" (
     echo found devkitPro
 )
 
-echo EXTRACT ISO FILES -----------------------------------------------
-gc_fst extract %ISO% || ( echo ERROR: ISO Extraction failed. & goto cleanup )
-gc_fst set-header "root/&&systemdata/ISO.hdr" "GTME01" "Melee Training Mode - More"
-
-echo MAKE SPACE IN ISO -----------------------------------------------
-del "root\MvHowto.mth" "root\MvOmake15.mth" "root\MvOpen.mth"
+if not exist "TM-More.iso" (
+    copy %ISO% "TM-More.iso"
+)
 
 echo BUILD C FILES --------------------------------------------------------
 
-mkdir "root\TM\"
+mkdir build
 
 echo build event menu
-copy "dats\eventMenu.dat" "root\TM\eventMenu.dat"
-"MexTK/MexTK.exe" -ff -i "src/events.c" -b "build" -s tmFunction -dat "root/TM/eventMenu.dat" -t "MexTK/tmFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'events.c' & goto cleanup )
-"MexTK/MexTK.exe" -trim "root/TM/eventMenu.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+copy "dats\eventMenu.dat" "build\eventMenu.dat"
+"MexTK/MexTK.exe" -ff -i "src/events.c" -b "build" -s tmFunction -dat "build/eventMenu.dat" -t "MexTK/tmFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'events.c' & goto cleanup )
+"MexTK/MexTK.exe" -trim "build/eventMenu.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
 echo build lab event
-copy "dats\lab.dat" "root\TM\lab.dat"
-"MexTK/MexTK.exe" -ff -i "src/lab.c" -b "build" -s evFunction -dat "root/TM/lab.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'lab.c' & goto cleanup)
-"MexTK/MexTK.exe" -trim "root/TM/lab.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+copy "dats\lab.dat" "build\lab.dat"
+"MexTK/MexTK.exe" -ff -i "src/lab.c" -b "build" -s evFunction -dat "build/lab.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'lab.c' & goto cleanup)
+"MexTK/MexTK.exe" -trim "build/lab.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
 echo build lab css
-copy "dats\labCSS.dat" "root\TM\labCSS.dat"
-"MexTK/MexTK.exe" -ff -i "src/lab_css.c" -b "build" -s cssFunction -dat "root/TM/labCSS.dat" -t "MexTK/cssFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'lab_css.c' & goto cleanup)
-"MexTK/MexTK.exe" -trim "root/TM/labCSS.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+copy "dats\labCSS.dat" "build\labCSS.dat"
+"MexTK/MexTK.exe" -ff -i "src/lab_css.c" -b "build" -s cssFunction -dat "build/labCSS.dat" -t "MexTK/cssFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'lab_css.c' & goto cleanup)
+"MexTK/MexTK.exe" -trim "build/labCSS.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
 echo build lcancel event
-copy "dats\lcancel.dat" "root\TM\lcancel.dat"
-"MexTK/MexTK.exe" -ff -i "src/lcancel.c" -b "build" -s evFunction -dat "root/TM/lcancel.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'lcancel.c' & goto cleanup)
-"MexTK/MexTK.exe" -trim "root/TM/lcancel.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+copy "dats\lcancel.dat" "build\lcancel.dat"
+"MexTK/MexTK.exe" -ff -i "src/lcancel.c" -b "build" -s evFunction -dat "build/lcancel.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'lcancel.c' & goto cleanup)
+"MexTK/MexTK.exe" -trim "build/lcancel.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
 echo build ledgedash event
-copy "dats\ledgedash.dat" "root\TM\ledgedash.dat"
-"MexTK/MexTK.exe" -ff -i "src/ledgedash.c" -b "build" -s evFunction -dat "root/TM/ledgedash.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'ledgedash.c' & goto cleanup)
-"MexTK/MexTK.exe" -trim "root/TM/ledgedash.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+copy "dats\ledgedash.dat" "build\ledgedash.dat"
+"MexTK/MexTK.exe" -ff -i "src/ledgedash.c" -b "build" -s evFunction -dat "build/ledgedash.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'ledgedash.c' & goto cleanup)
+"MexTK/MexTK.exe" -trim "build/ledgedash.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
 echo build wavedash event
-copy "dats\wavedash.dat" "root\TM\wavedash.dat"
-"MexTK/MexTK.exe" -ff -i "src/wavedash.c" -b "build" -s evFunction -dat "root/TM/wavedash.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'wavedash.c' & goto cleanup)
-"MexTK/MexTK.exe" -trim "root/TM/wavedash.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+copy "dats\wavedash.dat" "build\wavedash.dat"
+"MexTK/MexTK.exe" -ff -i "src/wavedash.c" -b "build" -s evFunction -dat "build/wavedash.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'wavedash.c' & goto cleanup)
+"MexTK/MexTK.exe" -trim "build/wavedash.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
 echo BUILD ASM FILES --------------------------------------------------------
 
@@ -79,32 +76,38 @@ gecko.exe build || (
     goto cleanup
 )
 cd ..
+copy "Additional ISO Files\codes.gct" "build\"
+copy "Additional ISO Files\opening.bnr" "build\"
+
 
 echo BUILD START.DOL --------------------------------------------------------
 
-copy "root\&&systemdata\Start.dol" "Start.dol"
-del "root\&&systemdata\Start.dol"
-"Build TM Start.dol\xdelta.exe" -d -f -s "Start.dol" "Build TM Start.dol\patch.xdelta" "root\&&systemdata\Start.dol" || ( echo ERROR: Failed to patch Start.dol & goto cleanup )
-del "Start.dol"
+gc_fst read %ISO% "Start.dol" "Start.dol"
+"Build TM Start.dol\xdelta.exe" -d -f -s "Start.dol" "Build TM Start.dol\patch.xdelta" "build\Start.dol" || ( echo ERROR: Failed to patch Start.dol & goto cleanup )
+del Start.dol
 
-echo COPYING EXTRA FILES ----------------------------------------------------
+echo BUILD ISO --------------------------------------------------------
+gc_fst fs TM-More.iso ^
+	delete MvHowto.mth ^
+	delete MvOmake15.mth ^
+	delete MvOpen.mth ^
+	insert TM\eventMenu.dat build\eventMenu.dat ^
+	insert TM\lab.dat build\lab.dat ^
+	insert TM\labCSS.dat build\labCSS.dat ^
+	insert TM\lcancel.dat build\lcancel.dat ^
+	insert TM\ledgedash.dat build\ledgedash.dat ^
+	insert TM\wavedash.dat build\wavedash.dat ^
+	insert codes.gct build\codes.gct ^
+	insert Start.dol build\Start.dol ^
+	insert opening.bnr build\opening.bnr
+gc_fst set-header TM-More.iso "GTME01" "Melee Training Mode - More"
 
-xcopy /s /e /y "Additional ISO Files\codes.gct" "root\"
-xcopy /s /e /y "Additional ISO Files\opening.bnr" "root\"
-
-echo REBUILD ISO --------------------------------------------------------
-gc_fst rebuild root "TM-More.iso" || ( echo ERROR: ISO rebuild failed. & goto cleanup )
 
 echo ############ TM-More.iso has been created ######################
 
 :cleanup
 
 echo CLEANUP -----------------------------------------------------------------
-
-if exist "root" (
-    echo deleting extracted files...
-    rmdir /s /q root\
-)
 
 if exist "build" (
     echo deleting build dir...
