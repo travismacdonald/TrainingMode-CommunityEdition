3b7103e2c8702f161e4c46ecf54f0d1b1d52aa6c
Alex Harrison <alexander.harrison.r@gmail.com>
2024-10-28 14:43:13 -0600

    fix ledgedash galint

--- ASM/training-mode/Onscreen Display/Ledge Codes/GALINT/Standalaone - GALINT Display.asm
+++ ASM/training-mode/Onscreen Display/Ledge Codes/GALINT/Standalaone - GALINT Display.asm
@@ -2,19 +2,10 @@
     .include "../../../Globals.s"
     .include "../../../../m-ex/Header.s"
 
-    .set entity, 31
     .set playerdata, 31
     .set player, 30
-    .set text, 29
     .set REG_isAerialInterrupt, 28
     .set REG_GALINT, 27
-
-##########################################################
-## 804a1f5c -> 804a1fd4 = Static Stock Icon Text Struct ##
-## Is 0x80 long and is zero'd at the start ##
-## of every VS Match ##
-## Store Text Info here ##
-##########################################################
 
     backupall
 
@@ -24,7 +15,6 @@
 
     # CHECK IF ENABLED
     li r0, OSD.Ledge                # PowerShield ID
-    # lwz r4, -0xdbc(rtoc) #get frame data toggle bits
     lwz r4, -0x77C0(r13)
     lwz r4, 0x1F24(r4)
     li r3, 1
@@ -52,79 +42,33 @@ CheckForFollower:
     stfd f1, 0x80(sp)
     lwz r3, 0x84(sp)
     sub REG_GALINT, REG_GALINT, r3
-
 SkipAI:
-    bl CreateText
 
-    # Change Text Color
+SetColor:
+    load r5, MSGCOLOR_RED
+    lhz r7, TM_TangibleFrameCount(playerdata)
+    neg r7, r7
     cmpwi REG_GALINT, 0x0
-    ble RedText
+    ble EndSetColor
+    load r5, MSGCOLOR_GREEN
+    mr r7, REG_GALINT
+EndSetColor:
 
-GreenText:
-    load r3, 0x8dff6eff             # green
-    b StoreTextColor
-
-RedText:
-    load r3, 0xffa2baff
-
-StoreTextColor:
-    stw r3, 0x30(text)
-
-    # Create Text
-    bl TopText
-    mr r3, r29                      # text pointer
-    mflr r4
-    lfs f1, -0x37B4(rtoc)           # default text X/Y
-    lfs f2, -0x37B4(rtoc)           # default text X/Y
-    branchl r12, 0x803a6b98
-
-    # Create Text2
-    bl BottomText
-    mr r3, r29                      # text pointer
-    mflr r4
-    mr r5, REG_GALINT
-    cmpwi r5, 0x0
-    bgt SkipShowingTangibleFrames
-
-    # Show Tangible Frames
-    lhz r5, TM_TangibleFrameCount(playerdata)
-    addi r5, r5, 0x1
-    neg r5, r5
-
-SkipShowingTangibleFrames:
-    lfs f1, -0x37B4(rtoc)           # default text X/Y
-    lfs f2, -0x37B0(rtoc)           # shift down on Y axis
-    branchl r12, 0x803a6b98
+    li r3, 19                   # ID
+    lbz r4, 0xC(playerdata)     # queue
+    bl Text
+    mflr r6
+    Message_Display
 
     b Exit
-
-CreateText:
-    mflr r0
-    stw r0, 0x0004(sp)
-    stwu sp, -0x0008(sp)
-    mr r3, playerdata               # backup playerdata pointer
-    li r4, 60                       # display for 60 frames
-    li r5, 0                        # Area to Display (0-2)
-    li r6, 14                       # Window ID(Unique to This Display)
-    branchl r12, TextCreateFunction # create text custom function
-    mr text, r3                     # backup text pointer
-    lwz r0, 0x000C(sp)
-    addi sp, sp, 8
-    mtlr r0
-    blr
 
 ###################
 ## TEXT CONTENTS ##
 ###################
 
-TopText:
+Text:
     blrl
-    .string "Ledgedash GALINT"
-    .align 2
-
-BottomText:
-    blrl
-    .string "Frames: %d"
+    .string "GALINT\nFrames: %d"
     .align 2
 
 ##############################
