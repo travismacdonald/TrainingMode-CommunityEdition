5c48eae2f54fcfe4da40535e446d40692fecaeda
Alex Harrison <alexander.harrison.r@gmail.com>
2024-10-27 20:20:02 -0600

    color hitboxes by ID

--- ASM/Additional Codes/Hitbox Color Changes Based On Damage.asm
+++ ASM/Additional Codes/Hitbox Color Changes Based On Damage.asm
@@ -1,72 +1,4 @@
     # To be inserted at 80009fa4
-    .macro branchl reg, address
-    lis \reg, \address @h
-    ori \reg, \reg, \address @l
-    mtctr \reg
-    bctrl
-    .endm
-
-    .macro branch reg, address
-    lis \reg, \address @h
-    ori \reg, \reg, \address @l
-    mtctr \reg
-    bctr
-    .endm
-
-    .macro load reg, address
-    lis \reg, \address @h
-    ori \reg, \reg, \address @l
-    .endm
-
-    .macro loadf regf, reg, address
-    lis \reg, \address @h
-    ori \reg, \reg, \address @l
-    stw \reg, -0x4(sp)
-    lfs \regf, -0x4(sp)
-    .endm
-
-    .macro backup
-    mflr r0
-    stw r0, 0x4(r1)
-    stwu r1, -0x100(r1)     # make space for 12 registers
-    stmw r20, 0x8(r1)
-    .endm
-
-    .macro restore
-    lmw r20, 0x8(r1)
-    lwz r0, 0x104(r1)
-    addi r1, r1, 0x100      # release the space
-    mtlr r0
-    .endm
-
-    .macro intToFloat reg, reg2
-    xoris \reg, \reg, 0x8000
-    lis r18, 0x4330
-    lfd f16, -0x7470(rtoc)  # load magic number
-    stw r18, 0(r2)
-    stw \reg, 4(r2)
-    lfd \reg2, 0(r2)
-    fsubs \reg2, \reg2, f16
-    .endm
-
-    .set ActionStateChange, 0x800693ac
-    .set HSD_Randi, 0x80380580
-    .set HSD_Randf, 0x80380528
-    .set Wait, 0x8008a348
-    .set Fall, 0x800cc730
-
-    .set entity, 31
-    .set player, 31
-    .set playerdata, 31
-
-# r0 is free
-# r5 is free
-
-# 18 = 0
-
-# 5 = 187
-
-# minimum GB value - (damage * (minimum GB value /max damage)
 
     # Get Values
     lfs f5, 0xC(r3)         # Get Hitbox Damage
@@ -77,45 +9,75 @@
     lfs f8, 0x8(r5)         # Damage Value for Darkest Color
 
     # Check If Does Over Max Damage Accounted For
+ClampToMax:
     fcmpo cr0, f5, f8
-    blt CheckMin
+    blt ClampToMin
     li r6, 0x0
-    b StoreGandB
+    b StoreGB
 
-CheckMin:
-    # Check If Does Under Min Damage Accounted For
+ClampToMin:
     fcmpo cr0, f5, f7
     bgt Formula
     lfs f8, 0x0(r5)         # Value for Lightest Color
     b ConvertToDecimal
 
-# Formula
 Formula:
     fsubs f8, f8, f7
     fsubs f5, f5, f7
     fsubs f7, f7, f7
-    fdivs f8, f6, f8        # (minimum GB value /max damage)
-    fmuls f8, f5, f8        # (damage * (minimum GB value /damage range)
-    fsubs f8, f6, f8        # minimum GB value - (damage * (minimum GB value /damage range)
+    fdivs f8, f6, f8        # minimum GB value / max damage
+    fmuls f8, f5, f8        # damage * (minimum GB value / damage range)
+    fsubs f8, f6, f8        # minimum GB value - (damage * (minimum GB value / damage range))
 
-# Convert To Decimal
 ConvertToDecimal:
     fctiwz f5, f8
     stfd f5, -0xC(sp)
     lwz r6, -0x8(sp)        # Get Hitbox Damage as Int
 
-# Store As G and B Value
-StoreGandB:
-    subi r5, r13, 32768     # Get Hitbox Color in DOL
+StoreGB:
+    subi r5, r13, 0x8000    # get hitbox color ptr
+
+    cmpwi r25, 0
+    beq ID_0
+    cmpwi r25, 1
+    beq ID_1
+    cmpwi r25, 2
+    beq ID_2
+    b ID_ETC
+
+ID_0:
+    stb r6, 0x1(r5)         # G
+    stb r6, 0x2(r5)         # B
+    li r6, 255
+    stb r6, 0x0(r5)         # R
+    b Alpha
+ID_1:
+    stb r6, 0x0(r5)         # R
+    stb r6, 0x2(r5)         # B
+    li r6, 255
+    stb r6, 0x1(r5)         # G
+    b Alpha
+ID_2:
+    subi r6, r6, 10
+    stb r6, 0x0(r5)         # R
+    stb r6, 0x1(r5)         # G
+    li r6, 255
+    stb r6, 0x2(r5)         # B
+    b Alpha
+ID_ETC:
+    stb r6, 0x0(r5)         # R
     stb r6, 0x1(r5)         # G
     stb r6, 0x2(r5)         # B
 
-    b exit
+Alpha:
+    li r6, 200
+    stb r6, 0x3(r5)         # A
+    b Exit
 
 Floats:
     blrl
-    .long 0x43580000        # Min GB Value 187
+    .long 0x43480000        # Min GB Value 200
     .long 0x40A00000        # Damage Value for Lightest Color 5
     .long 0x41900000        # Damage Value for Darkest Color 18
 
-exit:
+Exit:
