3831223ba8df0aea4df287c6c9487a0c7bf01ffb
Alex Harrison <alexander.harrison.r@gmail.com>
2024-10-28 14:25:05 -0600

    fix ledgestall

--- ASM/training-mode/Onscreen Display/Ledge Codes/Ledgestall/Ledgestall Display.asm
+++ ASM/training-mode/Onscreen Display/Ledge Codes/Ledgestall/Ledgestall Display.asm
@@ -2,12 +2,8 @@
     .include "../../../Globals.s"
     .include "../../../../m-ex/Header.s"
 
-    .set entity, 31
     .set playerdata, 31
     .set player, 30
-    .set text, 29
-    .set textprop, 28
-    .set hitbool, 27
 
 ##########################################################
 ## 804a1f5c -> 804a1fd4 = Static Stock Icon Text Struct ##
@@ -42,84 +38,41 @@ CheckForFollower:
     cmpwi r3, 15
     bgt Exit
 
-    bl CreateText
+SetColor:
+    load r5, MSGCOLOR_RED
+    bl TextVulnerable
+    mflr r6
 
-    # Change Text Color
     lhz r3, 0x2416(playerdata)
     subi r3, r3, 1
     cmpwi r3, 0
-    bgt RedText
+    bgt EndSetColor
 
-GreenText:
-    load r3, 0x8dff6eff             # green
-    b StoreTextColor
+    load r5, MSGCOLOR_GREEN
+    bl TextPerfect
+    mflr r6
+EndSetColor:
 
-RedText:
-    load r3, 0xffa2baff
-
-StoreTextColor:
-    stw r3, 0x30(text)
-
-    # Create Text
-    bl TopText
-    mr r3, r29                      # text pointer
-    mflr r4
-    lfs f1, -0x37B4(rtoc)           # default text X/Y
-    lfs f2, -0x37B4(rtoc)           # default text X/Y
-    branchl r12, 0x803a6b98
-
-    # Create Text2
-    bl BottomText_FramesLate
-    mr r3, r29                      # text pointer
-    mflr r4
-    lhz r5, 0x2416(playerdata)
-    subi r5, r5, 1
-    cmpwi r5, 0
-    bgt LateLedgestall
-
-    # Show Perfect Ledgestall Text
-    bl BottomText_PerfectStall
-    mflr r4
-
-LateLedgestall:
-    lfs f1, -0x37B4(rtoc)           # default text X/Y
-    lfs f2, -0x37B0(rtoc)           # shift down on Y axis
-    branchl r12, 0x803a6b98
+    li r3, 19                   # ID
+    lbz r4, 0xC(playerdata)     # queue
+    lhz r7, 0x2416(playerdata)
+    subi r7, r7, 1
+    Message_Display
 
     b Exit
-
-CreateText:
-    mflr r0
-    stw r0, 0x0004(sp)
-    stwu sp, -0x0008(sp)
-    mr r3, playerdata               # backup playerdata pointer
-    li r4, 60                       # display for 60 frames
-    li r5, 0                        # Area to Display (0-2)
-    li r6, 14                       # Window ID(Unique to This Display)
-    branchl r12, TextCreateFunction # create text custom function
-    mr text, r3                     # backup text pointer
-    lwz r0, 0x000C(sp)
-    addi sp, sp, 8
-    mtlr r0
-    blr
 
 ###################
 ## TEXT CONTENTS ##
 ###################
 
-TopText:
+TextVulnerable:
     blrl
-    .string "Ledgestall"
+    .string "Ledgestall\nVulnerable: %d"
     .align 2
 
-BottomText_FramesLate:
+TextPerfect:
     blrl
-    .string "Vulnerable: %d"
-    .align 2
-
-BottomText_PerfectStall:
-    blrl
-    .string "Perfect"
+    .string "Ledgestall\nPerfect"
     .align 2
 
 ##############################
