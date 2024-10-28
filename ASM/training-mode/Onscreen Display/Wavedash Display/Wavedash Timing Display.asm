57f666825af70c1bb3fe75cf7b5c423c9c266082
Alex Harrison <alexander.harrison.r@gmail.com>
2024-10-28 14:44:08 -0600

    remove comments from wavedash timing OSD

--- ASM/training-mode/Onscreen Display/Wavedash Display/Wavedash Timing Display.asm
+++ ASM/training-mode/Onscreen Display/Wavedash Display/Wavedash Timing Display.asm
@@ -95,45 +95,6 @@ ChangeAngleColor:
 
     b Injection_Exit
 
-    /*
-
-    # Create OSD Popup
-    mr r3, REG_FighterData          # backup REG_FighterData pointer
-    li r4, 60                       # display for 60 frames
-    li r5, 0                        # Area to Display (0-2)
-    li r6, 0                        # Window ID(Unique to This Display)
-    branchl r12, TextCreateFunction # create text custom function
-
-    mr text, r3                     # backup text pointer
-
-###########################
-## Print Wavedash Timing ##
-###########################
-
-    # Write out top line
-    mr r3, text                     # text pointer
-    bl TextASCII
-    mflr r4                         # get ASCII to print
-    lhz r5, TM_FramesinCurrentAS(REG_FighterData)
-    addi r5, r5, 1                  # 1-index number
-    lfs f1, -0x37B4(rtoc)           # default text X/Y
-    lfs f2, -0x37B4(rtoc)           # default text X/Y
-    crset 6
-    branchl r12, 0x803a6b98
-
-    # Check if frame perfect
-    lbz r4, 0x680(REG_FighterData)
-    cmpwi r4, 0x0
-    bne DisplayWavedashAngle
-    # Change Color
-    mr r4, r3                       # subtext id
-    mr r3, text                     # text pointer
-    load r5, 0x8dff6eff
-    stw r5, 0xF0(sp)
-    addi r5, sp, 0xF0
-    branchl r12, 0x803a74f0
-    */
-
 #########################
 ## Get Wavedash Angle ##
 #########################
@@ -201,50 +162,6 @@ SaveAngle:
     fmr f1, f2
     restore
     blr
-
-    /*
-    # Display Wavedash Angle
-    mr r3, text                     # text pointer
-    bl TextASCII2
-    mflr r4                         # get ASCII to print
-    lfs f1, -0x37B4(rtoc)           # default text X/Y
-    lfs f2, -0x37B0(rtoc)           # shift down on Y axis
-    lfs f3, 0x80(sp)
-    crset 6
-    branchl r12, 0x803a6b98
-
-    # Get Angle and Float Pointer
-    bl Floats
-    mflr r4
-    lfs f1, 0x80(sp)
-    # Check For Perfect Angle
-    lfs f2, 0x0(r4)
-    fcmpo cr0, f1, f2
-    blt Injection_Exit
-    lfs f2, 0x4(r4)
-    fcmpo cr0, f1, f2
-    blt PerfectAngle
-    lfs f2, 0x8(r4)
-    fcmpo cr0, f1, f2
-    bgt Injection_Exit
-
-OKAngle:
-    load r5, 0xfff000ff
-    stw r5, 0xF0(sp)
-    b ChangeAngleColor
-
-PerfectAngle:
-    load r5, 0x8dff6eff
-    stw r5, 0xF0(sp)
-    b ChangeAngleColor
-
-ChangeAngleColor:
-    # Change Color
-    mr r4, r3                       # subtext id
-    mr r3, text                     # text pointer
-    addi r5, sp, 0xF0
-    branchl r12, 0x803a74f0
-    */
 
 #########################
 
