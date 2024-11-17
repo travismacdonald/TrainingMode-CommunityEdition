54e014f045c636f0f7090942b4a03227b47f88ee
Alex Harrison <alexander.harrison.r@gmail.com>
2024-11-17 13:52:16 -0700

    prevent analog L from triggering frame advance

--- src/lab.c
+++ src/lab.c
@@ -2121,7 +2121,7 @@ int Update_CheckAdvance()
     int advance_btn = stc_advance_btns[LabOptions_General[OPTGEN_FRAMEBTN].option_val];
 
     // check if holding L
-    if (!LabOptions_General[OPTGEN_FRAMEBTN].disable && (pad->held & advance_btn) || (advance_btn == HSD_TRIGGER_L && pad->triggerLeft >= LOW_ANALOG_TRIGGER_THRESHOLD))
+    if (!LabOptions_General[OPTGEN_FRAMEBTN].disable && (pad->held & advance_btn))
     {
         timer++;
 
