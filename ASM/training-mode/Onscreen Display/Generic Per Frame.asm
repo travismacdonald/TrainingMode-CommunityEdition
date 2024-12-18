a4da05a53f6e5aee47182f8c82f7b4eb6ae8b829
Alex Harrison <alexander.harrison.r@gmail.com>
2024-12-18 11:54:24 -0700

    change up dtilt lockout osd

--- ASM/training-mode/Onscreen Display/Generic Per Frame.asm
+++ ASM/training-mode/Onscreen Display/Generic Per Frame.asm
@@ -285,14 +285,40 @@ OSD_Lockout:
     and. r0, r0, r4
     beq Exit
 
+#GetPrevLockout:
+#    bl GetPrevLockoutEnd
+#    .long 0
+#GetPrevLockoutEnd:
+#    mflr r3
+#    lwz r8, 0x0(r3)
+#    lbz r7, 0x674(playerdata) # timer_lstick_smash_y
+#    stw r7, 0x0(r3)
+
+    # dont show if in the air
     lwz r3, 0xE0(playerdata) # air_state
     cmpwi r3, 1
-    beq Lockout_LockoutEnded
+    beq Exit
 
-    lbz r7, 0x674(playerdata) # timer_lstick_smash_y
+    # dont show if in dtilt / dsmash
+    lwz r3, 0x10(playerdata) # state_id
+    cmpwi r3, ASID_AttackLw3
+    beq Exit
+    cmpwi r3, ASID_AttackLw4
+    beq Exit
+
+    # dont show if stick is vertical
+    lfs f0, 0x624(playerdata) # input.stick.y
+    fsubs f1, f1, f1 # zero f1
+    fcmpo cr0, f0, f1
+    bge Exit
+
+    lbz r7, 0x674(playerdata) # input.timer_lstick_smash_y
+    li r5, MSGCOLOR_RED
     cmpwi r7, 4
-    beq Lockout_LockoutEnded
-    bgt Exit
+    blt Lockout_InLockout
+    li r5, MSGCOLOR_GREEN
+    cmpwi r7, 25
+    bge Exit
 
 Lockout_InLockout:
     addi r7, r7, 1 # 1-index timer
@@ -306,7 +332,6 @@ Lockout_LockoutEnded:
 Lockout_Message_Display:
     li r3, OSD.LockoutTimers        # ID
     lbz r4, 0xC(playerdata)         # queue
-    load r5, MSGCOLOR_WHITE
     Message_Display
     b Exit
 
